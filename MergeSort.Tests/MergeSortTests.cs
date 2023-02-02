70570de34b5da1ba8680dd60c6f0148058612d8a
Stuart Shepherd <stuart@createstudios.com.au>
2023-02-03 08:18:52 +1100

    Add project files.

---
+++ MergeSort.Tests/MergeSortTests.cs
@@ -0,0 +1,16 @@
+namespace MergeSort.Tests;
+
+[TestClass]
+public class MergeSortTests
+{
+    [TestMethod]
+    [DataRow(new int[] { 73, 57, 49, 99, 133, 20, 1 }, new int[] { 1, 20, 49, 57, 73, 99, 133 })]
+    public void Sorts_Array_ReturnsSortedArray(int[] x, int[] expected)
+    {
+        var mergeSort = new MergeSort();
+        var actual = mergeSort.Sort(x, 0, x.Length - 1);
+
+        Assert.IsNotNull(actual);
+        CollectionAssert.AreEqual(actual, expected);
+    }
+}
